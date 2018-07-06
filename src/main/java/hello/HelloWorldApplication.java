b266f36c8aa3c92718bf4537e1a7ff2b784bd3cc
abcd <abcd@abcds-MacBook-Pro.local>
2018-07-06 12:48:13 +0530

    commit message

---
+++ src/main/java/hello/HelloWorldApplication.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by abcd on 06/07/18.
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String arg[]){
+
+		SpringApplication.run(HelloWorldApplication.class, arg);
+	}
+}
+
